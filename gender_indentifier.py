23637db0b6645c68d791b1150e386f5ce17ad565
Ahmed Azhar <ahmedazhar10@gmail.com>
2019-02-06 21:58:14 -0500

    Update gender_indentifier.py

--- gender_indentifier.py
+++ gender_indentifier.py
@@ -2,11 +2,6 @@ from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
-
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
@@ -15,12 +10,8 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-
-# CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([[190, 70, 43]])
-
-# CHALLENGE compare their reusults and print the best one!
 
 print(prediction)
